2007dcb58c924ee3614cf8ffa3a80fa2ccac4b98
fbtkd <fbtkd@DESKTOP-KBQQLQR>
2021-09-06 22:18:30 +0900

    초기 커밋.

---
+++ FlashPlayerSave/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기를 사용하여 수정하지 마십시오.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(74, 228)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(96, 28)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "백업"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(15, 40)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(317, 180)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "세이브 파일 목록"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(7, 18)
+        Me.ListBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(301, 154)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.ForestGreen
+        Me.Label1.Location = New System.Drawing.Point(19, 11)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(115, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "최근 백업일 :  -"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(176, 228)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(96, 28)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = "복구"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(345, 269)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "플래시게임 세이브 파일 백업 프로그램"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ FlashPlayerSave/Form1.vb
@@ -0,0 +1,52 @@
+﻿Imports System
+Imports System.IO
+Imports System.IO.Compression
+
+Public Class Form1
+
+    Dim path As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Macromedia\Flash Player\#SharedObjects"
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Dim di As New System.IO.DirectoryInfo(path)
+
+        For Each dir As System.IO.DirectoryInfo In di.GetDirectories()
+            ListBox1.Items.Add(dir)
+        Next
+
+        If My.Computer.FileSystem.FileExists(Application.StartupPath & "\FlashSaves.zip") Then
+            Label1.Text = "최근 백업일 : " & File.GetLastWriteTime(Application.StartupPath & "\FlashSaves.zip")
+        End If
+    End Sub
+
+    Private Sub ListBox1_MouseDoubleClick(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles ListBox1.MouseDoubleClick
+        Process.Start(path & "\" & ListBox1.SelectedItem.ToString)
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Button1.Text = "백업 중.."
+        If My.Computer.FileSystem.FileExists(Application.StartupPath & "\FlashSaves.zip") Then
+            System.IO.File.Delete(Application.StartupPath & "\FlashSaves.zip")
+        End If
+        ZipFile.CreateFromDirectory(path, "FlashSaves.zip")
+        Button1.Text = "백업"
+        MsgBox("백업이 완료되었습니다.", MsgBoxStyle.Information, "백업 완료")
+        Label1.Text = "최근 백업일 : " & File.GetLastWriteTime(Application.StartupPath & "\FlashSaves.zip")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Button2.Text = "복구 중.."
+        If My.Computer.FileSystem.FileExists(Application.StartupPath & "\FlashSaves.zip") Then
+            If Directory.Exists(path) Then
+                For Each file As String In Directory.GetDirectories(path, "*.*", SearchOption.TopDirectoryOnly)
+                    System.IO.Directory.Delete(file, True)
+                Next
+            End If
+        Else
+            MsgBox("백업된 FlashSaves.zip 파일을 찾을 수 없습니다.", MsgBoxStyle.Critical, "복구 실패")
+            Button2.Text = "복구"
+        End If
+        ZipFile.ExtractToDirectory(Application.StartupPath & "\FlashSaves.zip", path)
+        MsgBox("복구가 완료되었습니다.", MsgBoxStyle.Information, "복구 완료")
+        Button2.Text = "복구"
+    End Sub
+End Class
